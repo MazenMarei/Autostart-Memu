8771af4ab5e97b6cbb81f29b77d89950cfb19a3c
Mazen Marei Hassan Abdalla <108086157+MazenMarei@users.noreply.github.com>
2025-05-15 23:06:01 +0300

    Update config.ts

--- config.ts
+++ config.ts
@@ -1,10 +1,6 @@
-interface Config {
-    numberOFDevices: number | "auto"
-    delay: number
-}
 
 
 export default  {
     numberOFDevices: "auto", // or type a number
     delay: 1000 * 60 * 5,
-} satisfies Config;
+};
